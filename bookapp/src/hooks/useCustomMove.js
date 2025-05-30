d4949f8c056fe3eb6f6a56fa088f6a5f59ed6f34
Kim Dogyeong <pcs6389@gmail.com>
2025-05-30 12:47:45 +0900

    bookapp 예제 완성

--- bookapp/src/hooks/useCustomMove.js
+++ bookapp/src/hooks/useCustomMove.js
@@ -1,22 +1,29 @@
 //  페이지 나누기 이동
 
+import { useState } from "react";
 import {
   createSearchParams,
   useNavigate,
   useSearchParams,
-  pageParams,
 } from "react-router-dom";
 
+const getNum = (param, defaultValue) => {
+  if (!param) return defaultValue;
+  return parseInt(param);
+};
+
 const useCustomMove = () => {
+  const [refresh, setrefresh] = useState(false);
   const navigate = useNavigate();
+
   // 퀴리스트링 가져오기
   const [queryParams] = useSearchParams();
 
   // page=1&size=10&genre= 가져오기
-  const page = queryParams.get("page", 1);
-  const size = queryParams.get("size", 10);
-  const genre = queryParams.get("genre", 0);
-  const keyword = queryParams.get("keyword", "");
+  const page = getNum(queryParams.get("page"), 1);
+  const size = getNum(queryParams.get("size"), 10);
+  const genre = getNum(queryParams.get("genre"), 0);
+  const keyword = queryParams.get("keyword") || "";
 
   const queryDefault = createSearchParams({
     page,
@@ -32,15 +39,22 @@ const useCustomMove = () => {
     let queryStr = "";
 
     if (pageParam) {
-      const page = pageParams.get("page", 1);
-      const size = pageParams.get("size", 10);
-      const genre = pageParams.get("genre", 0);
-      const keyword = pageParams.get("keyword", "");
+      const page = getNum(pageParam.page, 1);
+      const size = getNum(pageParam.size, 10);
+      const genre = getNum(pageParam.genre, 0);
+      const keyword = pageParam.keyword || "";
 
       queryStr = createSearchParams({ page, size, genre, keyword }).toString();
     } else {
       queryStr = queryDefault;
     }
+
+    setrefresh(!refresh);
+
+    navigate({
+      pathname: `/`,
+      search: queryStr,
+    });
   };
 
   const moveToDetail = (id) => {
@@ -56,7 +70,16 @@ const useCustomMove = () => {
     });
   };
 
-  return { moveToList, moveToDetail, moveToEdit, page, size, genre, keyword };
+  return {
+    moveToList,
+    moveToDetail,
+    moveToEdit,
+    page,
+    size,
+    genre,
+    keyword,
+    refresh,
+  };
 };
 
 export default useCustomMove;
--- bookapp/src/pages/Home.jsx
+++ bookapp/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import BookList from "../components/BookList";
 import { genres } from "../util";
+import useCustomMove from "../hooks/useCustomMove";
 
 const Home = () => {
+  const moveState = useCustomMove();
+  // 검색창에서 사용할 정보 분해
+  const { size, genre, keyword, moveToList } = moveState;
   return (
     <div>
       <header className="flex mb-6">
@@ -10,15 +14,22 @@ const Home = () => {
         <div>
           <input
             type="text"
+            value={keyword}
+            onChange={(e) =>
+              moveToList({ page: 1, genre: genre, keyword: e.target.value })
+            }
             placeholder="Search by title or author"
             className="outline-0 p-2 border-2 border-gray-300 rounded-sm w-[200px] text-[.9em] leading-tight bg-white"
           />
           <select
+            value={genre}
             name="genre"
-            id=""
-            className="outline-0 p-2 border-gray-300 rounded-sm ml-2 text[.9em] leading-tight"
+            onChange={(e) =>
+              moveToList({ page: 1, genre: e.target.value, keyword: keyword })
+            }
+            className="outline-0 p-2 border-2 border-gray-300 rounded-sm ml-2 text[.9em] leading-tight bg-white"
           >
-            <option value="">All Genres</option>
+            <option value="0">All Genres</option>
             {genres.map((genre, idx) => (
               <option key={idx} value={idx + 1}>
                 {genre}
@@ -27,7 +38,7 @@ const Home = () => {
           </select>
         </div>
       </header>
-      <BookList />
+      <BookList moveState={moveState} />
     </div>
   );
 };
